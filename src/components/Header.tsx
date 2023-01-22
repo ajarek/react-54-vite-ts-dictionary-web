4242a58b79bc13d25d43080ad9aeef3e5482b36e
Ajarek <ajarek@poczta.onet.pl>
2023-01-22 22:46:40 +0100

    added css & logic to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { BiBookAlt } from 'react-icons/bi';
-import { BsSun, BsMoon } from 'react-icons/bs';
+import React, { useState } from 'react'
+import { BiBook } from 'react-icons/bi'
+import { BsSun, BsMoon } from 'react-icons/bs'
 const Header = () => {
+  const [thema, setThema] = useState(false)
   return (
     <div className='header'>
-      <div className="logo"><BiBookAlt/></div>
-      <div className="nav">
-        <select name="" id="">
-          <option  value="Serif">Serif</option>
-          <option value="Sans">Sans</option>
+      <div className='logo'>
+        <BiBook />
+      </div>
+      <div className='nav'>
+        <select
+          name=''
+          id=''
+        >
+          <option value='Serif'>Serif</option>
+          <option value='Sans'>Sans</option>
         </select>
-        <div className="check">
-         <div className="btn">
-         <button><span>o</span></button>
-         </div>
-         <div className="icon">
-         <BsSun/>
-         <BsMoon/>
-         </div>
-
+        <div className='check'>
+        <div className='icon'>{thema ? <BsSun /> :null }</div>
+          <div className='btn'>
+            <button onClick={() => setThema(!thema)}>
+              <span
+                style={
+                  thema
+                    ? { transform: 'translateX(35px)' }
+                    : { transform: 'translateX(0px)' }
+                }
+              ></span>
+            </button>
+          </div>
+          <div className='icon'>{thema ? null : <BsMoon />}</div>
         </div>
-
       </div>
     </div>
   )
