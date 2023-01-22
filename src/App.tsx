af38112f93db9579d9137b9dec8a81ffbbab29f6
Ajarek <ajarek@poczta.onet.pl>
2023-01-23 00:50:52 +0100

    added useFetch & Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, createContext } from 'react'
 import Header from './components/Header'
+import Input from './components/Input'
 export type GlobalContent = {
   thema: boolean
   setThema: (c: boolean) => void
@@ -9,12 +10,20 @@ export const AppContext = createContext<GlobalContent>({
   setThema: () => {},
 })
 function App() {
+  
   const [thema, setThema] = useState(false)
+  const [word, setWord] = useState('');
+  const url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+ 
  
   return (
     <div className={thema ? 'App dark' : 'App'}>
       <AppContext.Provider value={{ thema, setThema }}>
         <Header />
+        <Input
+         onChange={(e)=>setWord(e.target.value)}
+         value={word}
+        />
       </AppContext.Provider>
     </div>
   )
---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SlMagnifier } from 'react-icons/sl'
+
+type Props = {
+  value: string,
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+const Input = ({value, onChange}:Props) => {
+  return (
+    <div className='input'>
+      <input
+        type='search'
+        name=''
+        id=''
+        value={value}
+        onChange={onChange}
+      />
+      <div className='icon'>
+        <SlMagnifier />
+      </div>
+    </div>
+  )
+}
+
+export default Input
